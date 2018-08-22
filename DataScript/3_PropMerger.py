7683d08def2ef2583ec0103edb4fe14744118431
harikrishna <hariranpariya1990@gmail.com>
2018-08-22 01:15:25 -0700

    Updated Script
    Updated script for calulating overall property values of each trials

--- DataScript/3_PropMerger.py
+++ DataScript/3_PropMerger.py
@@ -16,10 +16,11 @@ class PropMerger:
 		self.batchSize=0
 		self.file_list = []
 		
-		self.FProp = [[0 for x in range(sensorCnt)] for y in range(propCnt)]
-		for prop in range (propCnt):
-			for sensor in range(sensorCnt):
-				self.FProp[prop][sensor] = []
+		self.FProp = [[[0 for x in range(self.sensorCnt)] for y in range(self.propCnt)] for y in range(self.sampleCnt)]
+		for trial in range(self.sampleCnt):
+			for prop in range (self.propCnt):
+				for sensor in range(self.sensorCnt):
+					self.FProp[trial][prop][sensor] = []
 	
 	def loadData(self, inputFile):
 		###printing file data
@@ -38,86 +39,97 @@ class PropMerger:
 		print(total_rows)
 		print(total_cols)
 		
-		for prop in range (self.propCnt):
-			for sensor in range(self.sensorCnt):
-				for batch in range(self.batchCnt):
-					self.FProp[prop][sensor].append(self.file_list[batch*self.sensorCnt+sensor+1][prop])
+		for trial in range (self.sampleCnt):
+			for prop in range (self.propCnt):
+				for sensor in range(self.sensorCnt):
+					for batch in range(self.batchCnt):
+						self.FProp[trial][prop][sensor].append(self.file_list[(trial*(self.batchCnt*self.sensorCnt))+(batch*self.sensorCnt+sensor+1)][prop])
 	
-	def netMean(self):
+	def findMean(self):
 		# Net Mean
-		for sensor in range(self.sensorCnt):
-			temp = 0.0
-			for batch in range(self.batchCnt):
-				temp += float(self.FProp[0][sensor][batch])
-			self.FProp[0][sensor].append(temp/self.batchCnt)
+		for trial in range(self.sampleCnt):
+			for sensor in range(self.sensorCnt):
+				temp = 0.0
+				for batch in range(self.batchCnt):
+					temp += float(self.FProp[trial][0][sensor][batch])
+				self.FProp[trial][0][sensor].append(temp/self.batchCnt)
 	
 	def findMedian(self):
 		# Median
 		median = []
-		for sensor in range(self.sensorCnt):
-			for batch in range(self.batchCnt):
-				median.append(self.FProp[1][sensor][batch])
-			median.sort()
-			mid_point = int(self.batchCnt/2)
-			if(mid_point%2==0):       
-				final_median = (float(median[mid_point-1])+float(median[mid_point]))/2
-				self.FProp[1][sensor].append(float(final_median))
-			else:
-				self.FProp[1][sensor].append(float(median[mid_point]))
-			median = []
+		for trial in range(self.sampleCnt):
+			for sensor in range(self.sensorCnt):
+				for batch in range(self.batchCnt):
+					median.append(self.FProp[trial][1][sensor][batch])
+				median.sort()
+				mid_point = int(self.batchCnt/2)
+				if(mid_point%2==0):       
+					final_median = (float(median[mid_point-1])+float(median[mid_point]))/2
+					self.FProp[trial][1][sensor].append(float(final_median))
+				else:
+					self.FProp[trial][1][sensor].append(float(median[mid_point]))
+				median = []
 	
 	def fingSTD(self):
 		# Standard deviation
-		for sensor in range(self.sensorCnt):
-			Stemp=0.0
-			for batch in range(self.batchCnt):
-				Stemp+=math.pow(float(self.FProp[4][sensor][batch]), 2)*(self.batchSize)
-			if (self.batchSize*self.batchCnt) > 0:
-				self.FProp[4][sensor].append(math.sqrt(Stemp/(self.batchSize*self.batchCnt)))
+		for trial in range(self.sampleCnt):
+			for sensor in range(self.sensorCnt):
+				Stemp=0.0
+				for batch in range(self.batchCnt):
+					Stemp+=math.pow(float(self.FProp[trial][4][sensor][batch]), 2)*(self.batchSize)
+				if (self.batchSize*self.batchCnt) > 0:
+					self.FProp[trial][4][sensor].append(math.sqrt(Stemp/(self.batchSize*self.batchCnt)))
 	
 	def findVariance(self):
 		# Variance = math.pow(std_dev, 2)
-		for sensor in range(self.sensorCnt):
-			self.FProp[5][sensor].append(math.pow(self.FProp[4][sensor][7], 2))
+		for trial in range(self.sampleCnt):
+			for sensor in range(self.sensorCnt):
+				self.FProp[trial][5][sensor].append(math.pow(self.FProp[trial][4][sensor][7], 2))
 	
 	def findRange(self):
 		# Range
-		for sensor in range(self.sensorCnt):
-			self.FProp[6][sensor].append(float(self.FProp[2][sensor][7])-float(self.FProp[3][sensor][7]))
+		for trial in range(self.sampleCnt):
+			for sensor in range(self.sensorCnt):
+				self.FProp[trial][6][sensor].append(float(self.FProp[trial][2][sensor][7])-float(self.FProp[trial][3][sensor][7]))
 	
 	def findSkewness(self):
 		# Skewness = (3*(net_mean - final_median))/std_dev
-		for sensor in range(self.sensorCnt):
-			self.FProp[7][sensor].append((3*(float(self.FProp[0][sensor][7]) - float(self.FProp[1][sensor][7])))/float(self.FProp[4][sensor][7]))
+		for trial in range(self.sampleCnt):
+			for sensor in range(self.sensorCnt):
+				self.FProp[trial][7][sensor].append((3*(float(self.FProp[trial][0][sensor][7]) - float(self.FProp[trial][1][sensor][7])))/float(self.FProp[trial][4][sensor][7]))
 	
 	def findKurtosis(self):
 		# Kurtosis = kurtosis_sum / math.pow(std_dev, 4)
-		for sensor in range(self.sensorCnt):
-			Ktemp=0.0
-			for batch in range(self.batchCnt):
-				Ktemp += float(self.FProp[8][sensor][batch])*self.batchSize*math.pow(float(self.FProp[4][sensor][batch]),4)
-			self.FProp[8][sensor].append(Ktemp/(math.pow(float(self.FProp[4][sensor][7]),4)*self.batchCnt*self.batchSize))
+		for trial in range(self.sampleCnt):
+			for sensor in range(self.sensorCnt):
+				Ktemp=0.0
+				for batch in range(self.batchCnt):
+					Ktemp += float(self.FProp[trial][8][sensor][batch])*self.batchSize*math.pow(float(self.FProp[trial][4][sensor][batch]),4)
+				self.FProp[trial][8][sensor].append(Ktemp/(math.pow(float(self.FProp[trial][4][sensor][7]),4)*self.batchCnt*self.batchSize))
 	
 	def saveData(self, outputFile):
 		# Copy final data (self.FProp[Prop][sensor][batch]) into a .csv file
 		with open(outputFile,"w") as my_final_file:
 			writer = csv.writer(my_final_file, lineterminator='\n')
-			writer.writerow(["Batch1","Batch2","Batch3","Batch4","Batch5","Batch6","Batch7","TotalAvg"])
-			for sensor in range(self.sensorCnt):
-				for prop in range(self.propCnt):
-					writer.writerow(self.FProp[prop][sensor])
+			writer.writerow(["Batch1","Batch2","Batch3","Batch4","Batch5","Batch6","Batch7","OverAll"])
+			for trial in range(self.sampleCnt):
+				for sensor in range(self.sensorCnt):
+					for prop in range(self.propCnt):
+						writer.writerow(self.FProp[trial][prop][sensor])
 	
 	def calAvgProp(self, inputFile, outputFile):
 		self.loadData(inputFile)
-		self.netMean()
+		self.findMean()
 		self.findMedian()
 		# Max
-		for sensor in range(self.sensorCnt):
-			self.FProp[2][sensor].append(max(list(map(float,self.FProp[2][sensor]))))
+		for trial in range(self.sampleCnt):
+			for sensor in range(self.sensorCnt):
+				self.FProp[trial][2][sensor].append(max(list(map(float,self.FProp[trial][2][sensor]))))
 
 		# Min
-		for sensor in range(self.sensorCnt):
-			self.FProp[3][sensor].append(min(list(map(float,self.FProp[3][sensor]))))
+		for trial in range(self.sampleCnt):
+			for sensor in range(self.sensorCnt):
+				self.FProp[trial][3][sensor].append(min(list(map(float,self.FProp[trial][3][sensor]))))
 		
 		self.fingSTD()
 	
